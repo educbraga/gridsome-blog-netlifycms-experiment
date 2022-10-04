24f3905ebf06392825ab2d60653a94edb8fcd2d2
Eduardo Braga <eduardocbraga@hotmail.com>
2022-10-04 18:33:17 -0300

    feat: consome api do reddit

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,12 +5,42 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
-  })
+// module.exports = function (api) {
+//   api.loadSource(({ addCollection }) => {
+//     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+//   })
 
-  api.createPages(({ createPage }) => {
-    // Use the Pages API here: https://gridsome.org/docs/pages-api/
+//   api.createPages(({ createPage }) => {
+//     // Use the Pages API here: https://gridsome.org/docs/pages-api/
+//   })
+// }
+
+const axios = require('axios')
+
+module.exports = function (api) {
+  api.loadSource(async actions => {
+    const { data } = await axios.get('https://www.reddit.com/r/aww.json?raw_json=1')
+
+    const collection = actions.addCollection('RedditPost')
+
+    api.createPages(({ createPage }) => {
+      createPage({
+        path: '/reddit/:id',
+        component: './src/templates/RedditPost.vue'
+      })
+    })  
+    
+
+    for (const post of data.data.children) {
+      collection.addNode({
+        id: post.data.id,
+        title: post.data.title,
+        path: '/reddit/'+ post.data.id,
+        fields: {
+          thumbnail: post.data.thumbnail,
+          // img: post.data.preview.images[0].source.url,
+        }
+      })
+    }
   })
 }
